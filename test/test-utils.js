b9dad1e304066dcab7ce9a9a185845acba885648
Jared Forsyth <jared@jaredforsyth.com>
2014-05-10 11:41:13 +0200

    some more tests

--- test/test-utils.js
+++ test/test-utils.js
@@ -38,5 +38,31 @@ describe('utils', function () {
       })
     })
   })
+
+  describe.only('hashJson', function () {
+    it('should hash something simple', function () {
+      expect(utils.hashJson({})).to.equal('{}')
+    })
+
+    it('should hash something complicated', function () {
+      expect(utils.hashJson({
+        b: 3,
+        c: 4,
+        a: [3,4,5,{y:10}]
+      })).to.eql('{"a": [3, 4, 5, {"y": 10}], "b": 3, "c": 4}')
+    })
+
+    it('should has same things the same', function () {
+      expect(utils.hashJson({
+        a: 3,
+        b: 5,
+        c: {a: 3, b: 10}
+      })).to.equal(utils.hashJson({
+        a: 3,
+        c: {b: 10, a: 3},
+        b: 5,
+      }))
+    })
+  })
 })
 
