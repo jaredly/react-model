bbdad4c48775859fc753ffcb9039e5c715dab965
Jared Forsyth <jared@jaredforsyth.com>
2014-05-10 10:31:56 +0200

    adding a test

---
+++ test/test-utils.js
@@ -0,0 +1,30 @@
+
+var expect = require('expect.js')
+  , utils = require('../lib/utils.js')
+
+var fixtures = {
+  stringParams: [
+    [
+      'complex example',
+      [{id: 'props.id', open: 'state.open', out: 'out', good: 'state.is.good'},
+        {id: 40, man: 10}, {open: 'yes', is: {good: 'awesome'}}, {out: 'in'}],
+      {
+        id: 40,
+        open: 'yes',
+        good: 'awesome',
+        out: 'in'
+      }
+    ]
+  ]
+}
+
+describe('utils', function () {
+  describe('stringParams', function () {
+    fixtures.stringParams.forEach(function (fix) {
+      it('should do ' + fix[0], function () {
+        expect(utils.stringParams.apply(null, fix[1])).to.eql(fix[2])
+      })
+    })
+  })
+})
+
